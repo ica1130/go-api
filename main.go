d54fe30e8daddf2d33592d0751dcae83e4c2bd92
ica1130 <ilijakrilovic@gmail.com>
2024-04-16 12:30:38 +0200

    feat: POST endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"rest-api/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +11,26 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080 for developement
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) //return json response, we return a status code and some data
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events) //return json response, we return a status code and some data
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event) //parse the request body into the event struct
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, event)
 }

b53a4e1d745653c8ae2d3878081482236381ac2c
ica1130 <ilijakrilovic@gmail.com>
2024-04-16 15:07:26 +0200

    refactor: Save event. Updated save event func to save the event to the db.

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"rest-api/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +16,22 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// later we will add it to a database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES(?, ?, ?, ?, ?)` // ? is a placeholder for the actual values but they are safe against SQL injection attacks
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id //set the ID field of the struct to the ID of the newly inserted row
+	return err
 }
 
 func GetAllEvents() []Event {
